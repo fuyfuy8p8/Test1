935f6708147e93b897a20ccc1143c1d88c5cb846
fuyfuy8p8 <101021113+fuyfuy8p8@users.noreply.github.com>
2023-04-19 20:48:18 +0300

    EnemySpawn

---
+++ EnemySpawn.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField] private Transform[] spawnPoint;
+    [SerializeField] private GameObject Enemy;
+
+
+    private void Update()
+    {
+        
+    }
+
+}
